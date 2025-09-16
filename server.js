5c2b29e23ddc13a91a924a5fc72a1a05f3fa4962
inoue290 <36177217+inoue290@users.noreply.github.com>
2025-09-16 21:49:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-// server.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
+
+// public フォルダを静的配信
+app.use(express.static("public"));
+
+// Render 用に http.Server を作成
 const server = http.createServer(app);
 
-// CORS 設定で全オリジン許可
+// CORS 設定を追加して WebSocket 接続を許可
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "*", // Render の URL でも OK
     methods: ["GET", "POST"]
   }
 });
-
-// public フォルダを静的配信
-app.use(express.static(path.join(__dirname, "public")));
 
 let players = {};
 const assetList = ["char1.png", "char2.png", "char3.png", "char4.png"];
@@ -28,57 +28,39 @@ io.on("connection", (socket) => {
   const randomAsset = assetList[Math.floor(Math.random() * assetList.length)];
   players[socket.id] = { x: 100, y: 100, hp: 100, asset: randomAsset };
 
-  // 接続したクライアントに現在のプレイヤー情報を送信
+  // 全プレイヤー送信
   socket.emit("state", players);
-
-  // 新規プレイヤーを他の全員に通知
   socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
 
   // 移動イベント
-  socket.on("move", (direction) => {
-    if (!players[socket.id]) return;
-
-    const speed = 10;
-    switch (direction) {
-      case "up": players[socket.id].y -= speed; break;
-      case "down": players[socket.id].y += speed; break;
-      case "left": players[socket.id].x -= speed; break;
-      case "right": players[socket.id].x += speed; break;
+  socket.on("move", (data) => {
+    if (players[socket.id]) {
+      players[socket.id].x += data.x;
+      players[socket.id].y += data.y;
+      io.emit("state", players); // 常に state を送信
     }
-
-    io.emit("playerMoved", { id: socket.id, ...players[socket.id] });
-  });
-
-  // クリックやタップで移動（任意）
-  socket.on("moveTo", (pos) => {
-    if (!players[socket.id]) return;
-    players[socket.id].x = pos.x;
-    players[socket.id].y = pos.y;
-    io.emit("playerMoved", { id: socket.id, ...players[socket.id] });
   });
 
   // ダメージイベント
   socket.on("hit", (targetId) => {
-    if (!players[targetId]) return;
-
-    players[targetId].hp -= 10;
-    if (players[targetId].hp <= 0) {
-      delete players[targetId];
-      io.emit("playerDead", targetId);
-    } else {
-      io.emit("playerHit", { id: targetId, hp: players[targetId].hp });
+    if (players[targetId]) {
+      players[targetId].hp -= 10;
+      if (players[targetId].hp <= 0) {
+        delete players[targetId];
+        io.emit("state", players);
+      } else {
+        io.emit("state", players);
+      }
     }
   });
 
-  // 切断
   socket.on("disconnect", () => {
     console.log("切断:", socket.id);
     delete players[socket.id];
-    io.emit("playerLeft", socket.id);
+    io.emit("state", players);
   });
 });
 
-// Render 用に process.env.PORT を使用
+// Render の環境変数 PORT を使う
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("サーバー起動:", PORT));
-
