2e150cabb1cf336217cd2d936b1ed43faa247c60
inoue290 <36177217+inoue290@users.noreply.github.com>
2025-09-16 22:08:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,42 +11,37 @@ app.use(express.static("public"));
 let players = {};
 const assetList = ["char1.png","char2.png","char3.png","char4.png"];
 
-io.on("connection", (socket) => {
-  console.log("接続:", socket.id);
+io.on("connection",(socket)=>{
+  console.log("接続:",socket.id);
 
-  // ランダムでアセット割り当て（文字列）
-  const randomAsset = assetList[Math.floor(Math.random() * assetList.length)];
-  players[socket.id] = { x:100, y:100, hp:100, asset: randomAsset };
+  // ランダムでアセット割り当て
+  const randomAsset = assetList[Math.floor(Math.random()*assetList.length)];
+  players[socket.id] = { x:100, y:100, hp:100, asset:randomAsset };
 
-  // 全プレイヤーを送信
-  io.emit("state", players);
+  // 新規プレイヤーを全員に通知
+  socket.emit("state", players);
+  socket.broadcast.emit("newPlayer", { id:socket.id, ...players[socket.id] });
 
-  // 移動
-  socket.on("move", (data) => {
+  // 移動イベント（スワイプ方向）
+  socket.on("move",(data)=>{
     if(players[socket.id]){
       players[socket.id].x += data.x;
       players[socket.id].y += data.y;
+
+      // 画面外に出ないよう制限
+      if(players[socket.id].x<0) players[socket.id].x=0;
+      if(players[socket.id].y<0) players[socket.id].y=0;
+
       io.emit("state", players);
     }
   });
 
-  // ダメージ
-  socket.on("hit", (targetId) => {
-    if(players[targetId]){
-      players[targetId].hp -= 10;
-      if(players[targetId].hp <=0){
-        delete players[targetId];
-      }
-      io.emit("state", players);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("切断:", socket.id);
+  socket.on("disconnect",()=>{
+    console.log("切断:",socket.id);
     delete players[socket.id];
     io.emit("state", players);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, ()=>console.log("サーバー起動:",PORT));
+server.listen(PORT,()=>console.log("サーバー起動:",PORT));
