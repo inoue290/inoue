1efbc11dc62dc65a4036f5baf7f82d3e3fc2be1a
inoue290 <36177217+inoue290@users.noreply.github.com>
2025-09-16 22:02:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,54 +3,41 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// public フォルダを静的配信
 app.use(express.static("public"));
 
-// Render 用に http.Server を作成
-const server = http.createServer(app);
-
-// CORS 設定を追加して WebSocket 接続を許可
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Render の URL でも OK
-    methods: ["GET", "POST"]
-  }
-});
-
 let players = {};
-const assetList = ["char1.png", "char2.png", "char3.png", "char4.png"];
+const assetList = ["char1.png","char2.png","char3.png","char4.png"];
 
 io.on("connection", (socket) => {
   console.log("接続:", socket.id);
 
-  // ランダムでアセット割り当て
+  // ランダムでアセット割り当て（文字列）
   const randomAsset = assetList[Math.floor(Math.random() * assetList.length)];
-  players[socket.id] = { x:100, y:100, hp:100, asset: randomIndex };
+  players[socket.id] = { x:100, y:100, hp:100, asset: randomAsset };
 
-  // 全プレイヤー送信
-  socket.emit("state", players);
-  socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
+  // 全プレイヤーを送信
+  io.emit("state", players);
 
-  // 移動イベント
+  // 移動
   socket.on("move", (data) => {
-    if (players[socket.id]) {
+    if(players[socket.id]){
       players[socket.id].x += data.x;
       players[socket.id].y += data.y;
-      io.emit("state", players); // 常に state を送信
+      io.emit("state", players);
     }
   });
 
-  // ダメージイベント
+  // ダメージ
   socket.on("hit", (targetId) => {
-    if (players[targetId]) {
+    if(players[targetId]){
       players[targetId].hp -= 10;
-      if (players[targetId].hp <= 0) {
+      if(players[targetId].hp <=0){
         delete players[targetId];
-        io.emit("state", players);
-      } else {
-        io.emit("state", players);
       }
+      io.emit("state", players);
     }
   });
 
@@ -61,7 +48,5 @@ io.on("connection", (socket) => {
   });
 });
 
-// Render の環境変数 PORT を使う
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("サーバー起動:", PORT));
-
+server.listen(PORT, ()=>console.log("サーバー起動:",PORT));
