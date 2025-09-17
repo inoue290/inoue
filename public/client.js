88be23987294e32883cc4ab99a54fed662c19edb
inoue290 <36177217+inoue290@users.noreply.github.com>
2025-09-17 20:54:48 +0900

    Update client.js

--- public/client.js
+++ public/client.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dx = t.clientX - startX;
     const dy = t.clientY - startY;
 
-    const scale = 0.3; // スワイプ距離→速度
+    const scale = 0.1; // スワイプ距離→速度
     socket.emit("move", { x: dx*scale, y: dy*scale });
   });
 
@@ -91,3 +91,4 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   animate();
 });
+
